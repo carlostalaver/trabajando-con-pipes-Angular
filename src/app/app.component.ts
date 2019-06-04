6b13e55d89613b65109e02606bb9de3422fd92a7
CARLOS <carlostalaver@gmail.com>
2019-06-04 19:30:18 -0400

    Trabajando con pipes de angular y personalizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  nombre = 'Carlos';
+  miArreglo: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  pi = Math.PI;
   title = 'spa';
+  salario = 1234.5
+  userLang = navigator.language;
+
+  object: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Llegó la data');
+    }, 3500);
+  });
+
+  fecha = new Date();
+  nombre2 = 'Hola cOmo  ESStan toDos';
+
+  video = 'TqnlZ79KYZ8';
+
+  contrasenna = '123456';
+  activar = true;
+  constructor() {
+    // alert ("The language is: " + this.userLang);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+// para traducir las fechas a español cuando se trabaja con el pipe date
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { CapitalicePipe } from './pipes/capitalice.pipe';
+import { DomSeguroPipe } from './pipes/dom-seguro.pipe';
+import { ContrasennaPipePipe } from './pipes/contrasenna-pipe.pipe';
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalicePipe,
+    DomSeguroPipe,
+    ContrasennaPipePipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' } // esta configuracion es para poder traducir las fechas a español
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
